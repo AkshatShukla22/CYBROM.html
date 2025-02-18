319a700bd1f9495a705edad995267d0c6a8643ec
AkshatShukla22 <akshatshukla8600@gmail.com>
2025-02-18 19:30:09 +0530

    first commit

---
+++ CPP/01.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<limits>
+using namespace std;
+
+int main(){
+    //data types
+    int a = 10; // integer
+    cout<<sizeof(bool);
+    cout<<&a;
+    cout<<numeric_limits<float>::max();
+}
