b868a51f058f9a44f36271bbbe0a04e64353565d
AkshatShukla22 <akshatshukla8600@gmail.com>
2025-02-28 17:55:36 +0530

    turnary operator

--- CPP/01.cpp
+++ CPP/01.cpp
@@ -5,13 +5,18 @@ using namespace std;
 int main(){
 
     int a = 5;
-    int b = 2;
+    int b;
     int c;
     cin>>c;
+    // b == c;
     // cout<<"enter number";
     // int b;
     // cin>>b;
 
-    (c == 1)? cout<< a*b : ( c == 2) ? cout<<a/b : cout<<"invalid";
+    // (c == 1)? cout<< a*b : ( c == 2) ? cout<<a/b : cout<<"invalid";
+
+    (c == a)? cout<<"matched" : cout<<"invalid try again",
+    cin>>b,
+    (b == a)? cout<<"matched" : cout<<"invalid try again" ;
 
 }
