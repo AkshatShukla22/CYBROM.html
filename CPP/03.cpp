b0b1b42fd066d9c73f98e8f4f7de72aed59b9a6c
AkshatShukla22 <akshatshukla8600@gmail.com>
2025-03-01 18:27:42 +0530

    something

---
+++ CPP/03.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cin>>a;
+    // char b;
+    // cin>>b;
+
+    // if( a>='A' && a<='Z' || a>='a' && a<='b' ){
+    //     cout<<"matched";
+    // }
+    // else{
+    //     cout<<"try again";
+    // }
+
+    // if( a>='A' && a<='Z')
+    //         cout<<"upper case";
+    
+    // else if(a>='a' && a<='b')
+    //     cout<<"lower case";
+    
+    // else
+    //     cout<<"invalid";   
+
+    if(a == 1 || a==3 || a== 5 || a==7 || a==10 || a==12){
+        cout<<"31 Days";
+    }
+    else if(a == 4 || a==6 || a== 8 || a==9 || a==11) {
+        cout<<"30 days";
+    }
+    else{
+        cout<<"28 days";
+    }
+    
+    
+}
