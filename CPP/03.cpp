34cee3bb411b31bc8f2f0af3e68faaa89f6a2e92
AkshatShukla22 <akshatshukla8600@gmail.com>
2025-03-03 11:49:47 +0530

    product banner complited

--- CPP/03.cpp
+++ CPP/03.cpp
@@ -23,15 +23,24 @@ int main(){
     // else
     //     cout<<"invalid";   
 
-    if(a == 1 || a==3 || a== 5 || a==7 || a==10 || a==12){
+    // if(a == 1 || a==3 || a== 5 || a==7 || a==10 || a==12){
+    //     cout<<"31 Days";
+    // }
+    // else if(a == 4 || a==6 || a== 8 || a==9 || a==11) {
+    //     cout<<"30 days";
+    // }
+    // else{
+    //     cout<<"28 days";
+    // }
+    
+    if(a%2 != 0){
         cout<<"31 Days";
     }
-    else if(a == 4 || a==6 || a== 8 || a==9 || a==11) {
-        cout<<"30 days";
-    }
-    else{
+    else if(a==2) {
         cout<<"28 days";
     }
-    
+    else{
+        cout<<"30 days";
+    }
     
 }
