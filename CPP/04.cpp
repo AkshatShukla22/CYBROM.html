63e70690c0d7fb853e14563743ae1200c03cb1b2
AkshatShukla22 <akshatshukla8600@gmail.com>
2025-03-08 19:35:22 +0530

    commiting

--- CPP/04.cpp
+++ CPP/04.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main(){
     int a;
     int b;
+    int c;
     cout<<"enter nambers";
     cin>>a;
     cin>>b;
-    char c;
     cin>>c;
+    // char c;
+    // cin>>c;
     
     // switch (a)
     // {
@@ -38,47 +41,83 @@ int main(){
     //     break;
     // }
 
-    switch (c)
-    {
-    case '+':
-        cout<<a+b;
-        break;
-    case '-':
-        cout<<a-b;
-        break;
-    case '*':
-        cout<<a*b;
-        break;
-    case '/':
-        cout<<a/b;
-        break;
-    case '%':
-        cout<<a%b;
-        break;
-    default:
-        cout<<"invalid";
-        break;
-    }
+    // switch (c)
+    // {
+    // case '+':
+    //     cout<<a+b;
+    //     break;
+    // case '-':
+    //     cout<<a-b;
+    //     break;
+    // case '*':
+    //     cout<<a*b;
+    //     break;
+    // case '/':
+    //     cout<<a/b;
+    //     break;
+    // case '%':
+    //     cout<<a%b;
+    //     break;
+    // default:
+    //     cout<<"invalid";
+    //     break;
+    // }
 
-    if(a==0){
-            cout<<"Sunday";
-        }
-    else if(c=='-'){
-            cout<<a+b;
-        }
-    else if(c=='-'){
-            cout<<a-b;
-        }
-    else if(c=='*'){
-            cout<<a*b;
-        }
-    else if(c=='/'){
-            cout<<a/b;
-        }
-    else if(c=='%'){
-            cout<<a%b;
-        }
-    else{
-            cout<<"Invalid";
-        }
+    // if(a==0){
+    //         cout<<"Sunday";
+    //     }
+    // else if(c=='-'){
+    //         cout<<a+b;
+    //     }
+    // else if(c=='-'){
+    //         cout<<a-b;
+    //     }
+    // else if(c=='*'){
+    //         cout<<a*b;
+    //     }
+    // else if(c=='/'){
+    //         cout<<a/b;
+    //     }
+    // else if(c=='%'){
+    //         cout<<a%b;
+    //     }
+    // else{
+    //         cout<<"Invalid";
+    //     }
+
+    // cout<<a*(a*b);
+    // int c = pow(a,b);
+    // if(c%10 == 7 ){
+    //     cout<<"Lucky";
+    // }
+    // else{
+    //     cout<<"UnLucky";
+    // }
+
+    // int d = (a*(a+1))/2;
+    // if(d%2 == 0){
+    //     cout<<"even";
+    // }
+    // else{
+    //     cout<<"odd";
+    // }
+
+    // if((a%400 == 0)||(a%4 == 0 && a%100 != 0)){
+    //     cout<<"leap year";
+    // }
+    // else{
+    //     cout<<"non leap year";
+    // }
+
+    int d = (b*b)-4*a*c;
+    cout<<d<<"\n";
+    if(d>0){
+        cout<<"real root and different";
+    }
+    else if(d==0){
+        cout<<"real root and equal";
+    }
+    else if (d<0){
+        cout<<"unreal root";
+    }
 }
