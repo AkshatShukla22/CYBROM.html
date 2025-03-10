2eb2472a0bf5f7a499487dcc21c76993f27d1ef8
AkshatShukla22 <akshatshukla8600@gmail.com>
2025-03-10 19:33:41 +0530

    commiting

--- CPP/04.cpp
+++ CPP/04.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 using namespace std;
 
 int main(){
-    int a;
-    int b;
-    int c;
-    cout<<"enter nambers";
-    cin>>a;
-    cin>>b;
-    cin>>c;
+    float a = 2.0/3;
+    // float b;
+    // int c;
+    // cout<<"enter nambers";
+    // cin>>a;
+    // cin>>b;
+    // cin>>c;
     // char c;
     // cin>>c;
     
@@ -109,15 +110,37 @@ int main(){
     //     cout<<"non leap year";
     // }
 
-    int d = (b*b)-4*a*c;
-    cout<<d<<"\n";
-    if(d>0){
-        cout<<"real root and different";
-    }
-    else if(d==0){
-        cout<<"real root and equal";
-    }
-    else if (d<0){
-        cout<<"unreal root";
-    }
+    // int d = (b*b)-4*a*c;
+    // cout<<d<<"\n";
+    // if(d>0){
+    //     cout<<"real root and different";
+    // }
+    // else if(d==0){
+    //     cout<<"real root and equal";
+    // }
+    // else if (d<0){
+    //     cout<<"unreal root";
+    // }
+
+    // if(a<100){
+    //     cout<<"no discount";
+    // }
+    // else if(a<500 && a>100){
+    //     cout<<"10 discount";
+    //     b=(a/100)*10;
+    //     cout<<a-b;
+    // }
+    // else if(a<1000 && a>500){
+    //     cout<<"15 discount";
+    //     b=(a/100)*15;
+    //     cout<<a-b;
+    // }
+    // else if(a<=1000 ){
+    //     cout<<"20 discount";
+    //     b=(a/100)*20;
+    //     cout<<a-b;
+    // }
+
+    cout<<fixed<<setprecision(10)<<a;
+    
 }
