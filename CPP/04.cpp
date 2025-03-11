d1fe2a278693f90fe431eba485fc84ffcd8d09f2
AkshatShukla22 <akshatshukla8600@gmail.com>
2025-03-11 23:58:22 +0530

    commiting

--- CPP/04.cpp
+++ CPP/04.cpp
@@ -4,15 +4,15 @@
 using namespace std;
 
 int main(){
-    float a = 2.0/3;
-    // float b;
-    // int c;
-    // cout<<"enter nambers";
-    // cin>>a;
-    // cin>>b;
+    int a;
+    float b;
+    float c;
+    cout<<"enter nambers: ";
+    cin>>a;
+    cin>>b;
     // cin>>c;
     // char c;
-    // cin>>c;
+    cin>>c;
     
     // switch (a)
     // {
@@ -141,6 +141,119 @@ int main(){
     //     cout<<a-b;
     // }
 
-    cout<<fixed<<setprecision(10)<<a;
+    // cout<<fixed<<setprecision(10)<<a;
+
+    // Q1
+    // if((b%100==0)&&(a>=b)){
+    //     cout<<"You can withdrawl\n"<<"your remaining amount is:"<<a-b;
+    // }
+    // else{
+    //     cout<<"Sorry you can not withdrawl";
+    // }
     
+    // Q2
+    // if(a>=90 && b>=75){
+    //     cout<<"100 scholorship";
+    // }
+    // else if(a>=80 && b>=60){
+    //     cout<<"60 scholorship";
+    // }
+    // else{
+    //     cout<<"No Scholarship";
+    // }
+
+    // Q3
+    // switch (a)
+    // {
+    // case 0:
+    //     cout<<"Zero";
+    //     break;    
+
+    // case 1:
+    //     cout<<"One";
+    //     break;
+
+    // case 2:
+    //     cout<<"Two";
+    //     break;
+
+    // case 3:
+    //     cout<<"Three";
+    //     break;
+
+    // case 4:
+    //     cout<<"Four";
+    //     break;
+
+    // case 5:
+    //     cout<<"Five";
+    //     break;
+
+    // case 6:
+    //     cout<<"Six";
+    //     break;
+
+    // case 7:
+    //     cout<<"Seven";
+    //     break;
+
+    // case 8:
+    //     cout<<"Eight";
+    //     break;
+
+    // case 9:
+    //     cout<<"Nine";
+    //     break;
+    
+    // default:
+    //     cout<<"Please enter a single digit number";
+    //     break;
+    // }
+
+    // Q4
+    // int d;
+    // int c;
+    // if(b<=12){
+    //     d = (a/100)*50;
+    //     cout<<"50% ""discount, now your price is: "<<a-d<<endl;
+    //     cout<<"no. of days flight is after: ";
+    //     cin>>c;
+    //     int e = (a-d)-((a-d)/100)*10;
+    //     if(c>=30){
+    //         cout<<"you got addetional 10% ""discount now your price is: "<<e;
+    //     }
+    //     else{
+    //         cout<<"sorry no more discount";
+    //     }
+    // }
+    // else if(b>=60){
+    //     d = (a/100)*30;
+    //     cout<<"30% ""discount, now your price is: "<<a-d<<endl;
+    //     cout<<"no. of days flight is after: ";
+    //     cin>>c;
+    //     int e = (a-d)-((a-d)/100)*10;
+    //     if(c>=30){
+    //         cout<<"you got addetional 10% ""discount now your price is: "<<e;
+    //     }
+    //     else{
+    //         cout<<"sorry no more discount";
+    //     }
+    // }
+    // else{
+    //     cout<<"no discount";
+    // }
+
+    // Q5
+    int d;
+    if(b>5 && c>=4){
+       d =(a/100)*20;
+       cout<<"you got 20% bonus, which is: "<<d;
+    }
+    else if((b<=5 && b>=3) && c>=3){
+        d = (a/100)*10;
+        cout<<"you got 10% bonus, which is: "<<d;
+     }
+    else{
+        cout<<"no bonus, work hard";
+    }
 }
