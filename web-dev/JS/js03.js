808bbfe0cc516e04f3377488af7b4c0c09b960ee
AkshatShukla22 <akshatshukla8600@gmail.com>
2025-03-29 18:32:55 +0530

    commiting

---
+++ web-dev/JS/js03.js
@@ -0,0 +1,21 @@
+var a = 12;
+var b = 13
+console.log(`the sum of `+a+ ` and ` +b+ ` is: `, a + b);
+
+//function
+function something() { //function decleration
+    var c = 5;
+    var d = 6;
+    console.log(c+d);
+}
+something(); //calling the function
+
+
+//arrow function
+arrow=()=>{  //declaring arrow function
+    var e = 4;
+    var f = 6;
+    console.log(e+f);
+}
+arrow(); //calling  arrow function
+
