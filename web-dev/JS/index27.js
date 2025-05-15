371c99829b42aec724ebef3476b3b86f40a2a0fe
AkshatShukla22 <akshatshukla8600@gmail.com>
2025-05-15 18:54:43 +0530

    done

---
+++ web-dev/JS/index27.js
@@ -0,0 +1,55 @@
+
+        // Q1
+        // let a = "dog";
+        // let len = a.length;
+        // for(let i= 0; i<=len; i++){
+        //     for(let j = i+1; j<=len; j++){
+        //         console.log(a.slice(i,j));
+        //     }
+        // }
+
+        // Q2
+        // let a = "The quick brown fox";
+        // let len = a.length;
+        // let count = 0;
+        // for(let i = 0; i<=len; i++){
+        //     let j = i+1;
+        //     let b = a.slice(i,j);
+        //     if(b==="a" || b==="e" || b==="i" || b==="o" || b==="u"){
+        //         count ++;
+        //     }
+        // }
+        // console.log(count);
+
+        // Q3
+        // fun1 = () => {
+        //     let a = "Hello";
+        //     console.log(a);
+        // }
+        // fun2 = (fun1) => {
+        //     setTimeout(fun1,2000);
+        // }
+        // fun2(fun1);
+
+        // Q.7
+        // import greet from './index26.js';
+        // console.log(greet("Akshat"));
+
+        // Q.8
+        import { add, subtract, multiply, divide } from './index26.js';
+
+        console.log(add(10, 5)); 
+        console.log(subtract(10, 5)); 
+        console.log(multiply(10, 5));
+        console.log(divide(10, 5));   
+
+
+        // Q.9
+        // import data from './index26.json' assert { type: 'json' };
+        // console.log(data.name);
+        // console.log(data.age); 
+
+        // Q.10
+        // let myColor = ["Red", "Green", "White", "Black"];
+        // let result = myColor.join(",");
+        // console.log(result);
