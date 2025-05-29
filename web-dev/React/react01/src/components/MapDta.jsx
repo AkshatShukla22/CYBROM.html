7f741219a558fa1af30b2d435900b71727cbcd6f
AkshatShukla22 <akshatshukla8600@gmail.com>
2025-05-29 22:32:23 +0530

    done

---
+++ web-dev/React/react01/src/components/MapDta.jsx
@@ -0,0 +1,38 @@
+
+
+    let student = [
+        {
+            name: "Akshat Shukla",
+            age: 21,
+        },
+        {
+            name: "Irfan Lohar",
+            age: 21,
+        },
+        {
+            name: "Chetan Doniwal",
+            age: 21,
+        },
+        {
+            name: "Hritik Shukla",
+            age: 22,
+        }
+    ]
+    export default student;
+    
+    // const MapDta = () => {
+    //     const ans = student.map((key)=>{
+    //         return (
+    //           <>
+    //             <tr>
+    //             <td>{key.name}</td>
+    //             <td>{key.age}</td>
+    //             </tr>
+    //           </>
+    //         );
+    //     })
+    //     return(ans)
+    // } 
+    // export default MapDta;
+    
+
