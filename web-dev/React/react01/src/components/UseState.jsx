0613f926166d2db55b52e34e9f623a88c6f7b7d6
AkshatShukla22 <akshatshukla8600@gmail.com>
2025-06-03 18:24:44 +0530

    done

---
+++ web-dev/React/react01/src/components/UseState.jsx
@@ -0,0 +1,39 @@
+import { use, useState } from "react";
+
+const App = () => {
+  // const [name, setName ] = useState("Akshat");
+
+  // const [color, setColor] = useState ("red");
+
+    //   const [count, setCount] = useState(0);
+    //   const myinc = () => {
+    //     setCount(count + 1);
+    //   };
+    //   const mydec = () => {
+    //     if(count==0){
+    //       alert("Cant do that");
+    //     }
+    //     else{
+    //       setCount(count - 1); 
+    //     }
+    //   };
+
+  return(
+    <>
+      {/* <h1>Welcome  to React App: {name} </h1>
+      <button onClick={()=>{setName("Naruto")}}>Click me</button> */}
+
+      {/* <button onClick={()=>{setColor("yellow")}}>Yellow</button> 
+      <button onClick={()=>{setColor("blue")}}>Blue</button> 
+      <button onClick={()=>{setColor("green")}}>Green</button> 
+      <div style={{color: color, fontSize: "3vh"}}>{color}</div> */}
+
+      {/* <h1>Counter App</h1>
+      <button onClick={myinc}>Increment</button>
+      <h1>{count}</h1> 
+      <button onClick={mydec}>Decrement</button> */}
+    </>
+  ) 
+}
+
+export default App;
