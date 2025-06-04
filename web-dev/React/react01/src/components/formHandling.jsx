8fa44cfcbc49c0acc65c2e7c5b01f48c93fb4d1c
AkshatShukla22 <akshatshukla8600@gmail.com>
2025-06-04 18:29:22 +0530

    done

--- web-dev/React/react01/src/components/formHandling.jsx
+++ web-dev/React/react01/src/components/formHandling.jsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
 
 const App = () => {
-  const [ name, setName ] = useState("");
+//   const [ name, setName ] = useState("");
+//   const [ city, setCity ] = useState("");
+//   const handleSubmit = ()=>{
+//     console.log({name:name, city,city});
+//   }
+
+const [ name, setName ] = useState("");
   const [ city, setCity ] = useState("");
   const [ rno, setRno ] = useState("");
   const [ fees, setFees ] = useState("");
@@ -12,16 +18,21 @@ const App = () => {
     setInput(values=>({...values, [name]: value}))
     console.log(input)
   }
-  
 
   return (
     <>
-      <h1>Application Form</h1>
+      {/* <h1>Application Form</h1>
+      Enter Name: <input type="text"  value={name} onChange={(e)=>{setName(e.target.value)}} /><br/>
+      Enter City: <input type="text" value={city} onChange={(e)=>{setCity(e.target.value)}} /><br/>
+      <button onClick={handleSubmit}>Click Me</button> */}
+
+        <h1>Application Form</h1>
       Enter Name: <input type="text" name="name" onChange={handleInput} /><br/>
       Enter City: <input type="text" name="city" onChange={handleInput} /><br/>
       Enter Roll no: <input type="text" name="rollno" onChange={handleInput} /><br/>
       Enter Fees: <input type="text" name="fees" onChange={handleInput} /><br/>
       <button >Click Me</button>
+
     </>
   );
 };
