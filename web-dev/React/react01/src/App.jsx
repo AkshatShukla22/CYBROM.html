de6a527108bea777066c1c23b8477a4dc587fee6
AkshatShukla22 <akshatshukla8600@gmail.com>
2025-06-16 18:28:37 +0530

    done

--- web-dev/React/react01/src/App.jsx
+++ web-dev/React/react01/src/App.jsx
@@ -1,13 +1,50 @@
-import Cybrom02 from "./components/Cybrom02";
+import { useMemo, useState } from "react";
+
+// const App = () => {
+//   const [add, setAdd] = useState(0);
+//   const [minus, setMinus] = useState(100);
+//   const multi = useMemo(()=>{
+//     console.log("*****")
+//     return add*2
+//   }, [add]);
+//   return (
+//     <>
+//     <h1>Welcome</h1>
+//     <h1>Multiplication: {multi}</h1>
+//     <h1>Addition: {add}</h1>
+//     <h1>Substraction: {minus}</h1>
+//     <button onClick={()=>{setAdd(add+1)}}>Add</button>
+//     <button onClick={()=>{setMinus(minus-1)}}>Minus</button>
+//     </>
+//   );
+// };
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [city, setCity] = useState("");
+  const ExpansiveVAl = useMemo(() =>{
+    for(var i= 0; i<10000000; i++){}
+    return num*2;
+  },[num])
+
+  // const ExpensiveCityVal2 = useMemo(() => {
+  //   let result = 0;
+  //   for (let i = 0; i < 1000000000; i++) {}
+  //   return city.toUpperCase();
+  // }, [city]);
+
   return (
-    <div>
-      <h1>Welcome My App!</h1>
-      <Cybrom02>
-        <h3>We learn Java Script</h3>
-      </Cybrom02>
-    </div>
+    <>
+    <h1>Welcome</h1>
+    Select Number : <input type="number" value={num} onChange={(e)=>{
+      setNum(e.target.value);
+    }} />
+    Entr City : <input type="text" value={city} onChange={(e)=>{
+      setCity(e.target.value);
+      // ExpensiveCityVal2;
+    }} />
+    <h2>My Count : {ExpansiveVAl}</h2>
+    </>
   );
 };
 
