31c95c8f77f92e598c2b1b7c8262bd9ee71c8b5e
AkshatShukla22 <akshatshukla8600@gmail.com>
2025-06-21 18:25:08 +0530

    done

--- web-dev/React/react01/src/App.jsx
+++ web-dev/React/react01/src/App.jsx
@@ -1,10 +1,18 @@
-import UseRef from "./components/UseRef";
+import { useSelector, useDispatch } from "react-redux";
+import { bgcolorChange } from "./colorSlice";
+import{ useState } from 'react'
 
 const App = () => {
+  const clr=useSelector(state=>state.mycolor.color);
+  const dispatech = useDispatch();
+  const [txtval, setTxtVal] = useState("");
 
   return (
     <>
-    <UseRef />
+    <h1> Welcome </h1>
+    Enter Color: <input type="text" value={txtval} onChange={(e)=>{setTxtVal(e.target.value)}} />
+    <button onClick={()=>{dispatech(bgcolorChange(txtval))}}>Change Color</button>
+    <div style={{width:"400px", height:"200px", backgroundColor:clr }}></div>
     </>
   );
 };
---
+++ web-dev/React/react01/src/Store02.jsx
@@ -0,0 +1,9 @@
+// Store02.jsx
+import { configureStore } from "@reduxjs/toolkit";
+import myReducer from './colorSlice';
+const store = configureStore({
+    reducer:{
+        mycolor:myReducer
+    }
+})
+export default store;
