ddd80d7cb472539b36086a885acbc5057d0f21fd
AkshatShukla22 <akshatshukla8600@gmail.com>
2025-08-16 19:25:58 +0530

    done

--- web-dev/node/index10/frontend/src/utils/BackendUrl.jsx
+++ web-dev/node/index10/frontend/src/utils/BackendUrl.jsx
@@ -1,4 +1,2 @@
 const BackendUrl = 'http://localhost:8000/';
 export default BackendUrl;
-//  This file contains the base URL for the backend API.
-//  It is used to make requests to the backend server.
